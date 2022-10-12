7161ba822a89d180c8cc41a51ea5df4189e39ca1
Allan Coutarel <allancoutarel@orange.fr>
2022-10-12 23:16:38 +0200

    v2 for the timer

--- js/chrono.js
+++ js/chrono.js
@@ -5,53 +5,64 @@ let nbr_exos = document.querySelector('#nbr-exos');
 let num_exo = document.querySelector("#num-exo");
 let num_serie = document.querySelector("#num-serie");
 
-num_exo.max = nbr_exos.value;
-num_serie.max = nbr_series.value;
+
+let date;
+let minutes;
+let seconds;
+let timer;
+let whistle;
+
 
 nbr_exos.addEventListener("change", () => {
     num_exo.max = nbr_exos.value;
+    num_exo.value = num_exo.defaultValue;
+    document.querySelector(".num-exo-btn").style.visibility = "visible";
 })
 
 nbr_series.addEventListener("change", () => {
     num_serie.max = nbr_series.value;
+    num_serie.value = num_serie.defaultValue;
+    document.querySelector(".num-serie-btn").style.visibility = "visible";
 })
 
-let whistle = new Audio('../assets/whistle.mp3');
-
-let date;
-let hours;
-let minutes;
-let seconds;
-let timer;
 
 num_exo.addEventListener("change", () => {
-    document.querySelector("#chrono").style.backgroundColor = "#222";
     num_serie.value = num_serie.defaultValue;
+    document.querySelector(".num-serie-btn").style.visibility = "visible";
 
     if(timer!==null){
         clearInterval(timer);
     }
-    if(num_serie != 0) {
+    if(num_exo.value > 0 && num_exo.value <= num_exo.max) {
         date = time_exo.value.split(':');
-        hours = parseInt(date[0]);
-        minutes = parseInt(date[1]);
-        seconds = parseInt(date[2]);
+        minutes = parseInt(date[0]);
+        seconds = parseInt(date[1]);
+        document.querySelector("#chrono").style.backgroundColor = "red";
+        chrono();
         timer = setInterval(chrono, 1000);
+        //setTimeout( () => { document.querySelector("#chrono").style.backgroundColor = "red"; }, 1000);
+    }
+    if(num_exo.value == num_exo.max) {
+        document.querySelector(".num-exo-btn").style.visibility = "hidden";
     }
 })
 
 num_serie.addEventListener("change", () => {
-    document.querySelector("#chrono").style.backgroundColor = "#222";
 
     if(timer!==null){
         clearInterval(timer);
     }
-    if(num_serie != 0) {
+    if(num_serie.value > 0 && num_serie.value <= num_serie.max) {
         date = time_serie.value.split(':');
-        hours = parseInt(date[0]);
-        minutes = parseInt(date[1]);
-        seconds = parseInt(date[2]);
+        minutes = parseInt(date[0]);
+        seconds = parseInt(date[1]);
+        document.querySelector("#chrono").style.backgroundColor = "red";
+        chrono();
         timer = setInterval(chrono, 1000);
+        //setTimeout( () => { document.querySelector("#chrono").style.backgroundColor = "red"; }, 1000);
+    }
+    if(num_serie.value == num_serie.max) {
+        document.querySelector(".num-serie-btn").style.visibility = "hidden";
     }
 })
 
@@ -62,20 +73,23 @@ function chrono() {
         seconds = 59;
         minutes--;
     }
-    
-    if(minutes == 0 && seconds == 0){
-        clearInterval(timer);
-        document.querySelector("#chrono").style.backgroundColor = "red";
-        whistle.play();
-    }
 
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
 
     document.querySelector("#chrono").innerHTML = ` ${m} : ${s}`;
+    
+    if(minutes == 0 && seconds == 0){
+        clearInterval(timer);
+        whistle.play();
+        document.querySelector("#chrono").style.backgroundColor = "green";
+        document.querySelector("#chrono").innerHTML = "Let's go !";
+    }
 }
 
 
 window.addEventListener("load", () => {
-
+    whistle = new Audio('../assets/whistle.mp3');
+    num_exo.max = nbr_exos.value;
+    num_serie.max = nbr_series.value;
 });
